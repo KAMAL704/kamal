8a7b97cd8a454ed673d0493724e47230e312e71b
kamal <kamalbajiya15@gmail.com>
2026-03-02 14:19:05 +0530

    f

--- script.js
+++ script.js
@@ -33,3 +33,53 @@ function deletingEffect() {
 }
 
 typingEffect();
+AOS.init();
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 60 },
+    size: { value: 3 },
+    move: { speed: 2 },
+    line_linked: { enable: true },
+  }
+});
+// Theme Toggle
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if(document.body.classList.contains("dark-mode")){
+    toggleBtn.innerText = "☀️";
+  } else {
+    toggleBtn.innerText = "🌙";
+  }
+});
+// Visitor Counter
+if(localStorage.getItem("visits")){
+  localStorage.setItem("visits",
+  Number(localStorage.getItem("visits")) + 1);
+}else{
+  localStorage.setItem("visits", 1);
+}
+
+document.getElementById("visitorCount").innerText =
+localStorage.getItem("visits");
+const chatInput = document.getElementById("chatInput");
+const chatBody = document.getElementById("chatBody");
+
+chatInput.addEventListener("keypress", function(e){
+  if(e.key === "Enter"){
+    const msg = chatInput.value;
+    chatBody.innerHTML += "<p><strong>You:</strong> " + msg + "</p>";
+
+    let reply = "Thanks for visiting Kamal's portfolio!";
+
+    if(msg.toLowerCase().includes("skills")){
+      reply = "Kamal has skills in Cybersecurity, Python and Kali Linux.";
+    }
+
+    chatBody.innerHTML += "<p><strong>Bot:</strong> " + reply + "</p>";
+    chatInput.value = "";
+  }
+});
